4a9783f52c44848a9ddf0020aed06fd3b6e1fd1a
Ashton Redd <redlink2@pm.me>
2021-11-01 19:59:13 -0500

    Refactor Nuxt(V2) -> Nuxt(V3)
    Changes include:
    -Full change to version 3 of NuxtJS
    -Favicon fixed
    -Title changed

---
+++ components/lefty.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="lefty"></div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		const randy = Math.floor(Math.random() * this.ideas.length);
+		document.querySelector('#lefty').innerHTML = `<h1>` + this.ideas[randy] + `</h1>`;
+	},
+	data() {
+		return {
+			ideas: [
+				'make shapes = links',
+				'better background color',
+				'buttons to change righty an lefty',
+				'hueshift on the shapes with random levels?',
+				'better assets to use besides builtin buttons',
+				'maybe there should be a picture here that gets changed?'
+			],
+		}
+	}
+}
+</script>
+
+<style scoped>
+#lefty {
+	height: 100%;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	font-size: 3vh;
+	color: var(--subGreen);
+}
+</style>
---
+++ components/righty.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="contain">
+		<div id="link1"></div>
+		<div id="link2"></div>
+		<div id="link3"></div>
+	</div>
+</template>
+
+<style scoped>
+#contain {
+	height: 100%;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-evenly;
+}
+</style>
