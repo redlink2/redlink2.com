8d416ff27c8ebd047a738ac19873d6133380749e
Ashton Redd <redlink2@pm.me>
2021-11-14 22:13:52 -0600

    Added card generator to game.vue.
    -randomly select a card and display it

--- pages/game.vue
+++ pages/game.vue
@@ -2,9 +2,11 @@
 <body>
     <div>
         <div id="mat">
-            <div id="opchoice">{{ opcard }}</div>
-            <div id="urcard">{{ pcard }}</div>
-            <div id="hand">{{ hand }}</div>
+            <div id="opchoice"></div>
+            <div id="urcard">
+                <img :src="require(`~/assets/pc/${pcard}/${number}.png`)" />
+            </div>
+            <div id="hand"></div>
         </div>
     </div>
 </body>
@@ -12,20 +14,16 @@
 <script>
 export default {
     mounted() {
-        let range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-        const length = range.length;
-        let oprandy = Math.floor(Math.random() * length)
-        let prandy = Math.floor(Math.random() * length)
-        let sel = 0
-        this.opcard = range[oprandy]
-        this.pcard = range[prandy]
-        this.hand = oprandy
+        let opt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+        const length = opt.length
+        let sel = Math.floor(Math.random() * length)
+        this.number = opt[sel]
     },
     data() {
         return {
-            opcard: 0,
-            pcard: 0,
-            hand: 0,
+            suit: "",
+            number: 0,
+            pcard: "spade"
         }
     },
 }
