11425a586147bb184b2fb64711257d3a757e59b8
Ashton Redd <redlink2@pm.me>
2021-11-22 01:29:35 -0600

    Changes to toggle.vue.
    -soon to be theme changer that will change between var(--bg) and var(--bgPurple)

--- components/toggle.vue
+++ components/toggle.vue
@@ -1,9 +1,51 @@
+<template>
+    <div id="switchContain">
+        <div id="displaced" v-on:click="colorShift">displace!</div>
+    </div>
+</template>
+<style>
+#switchContain {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#displaced {
+    padding: 10%;
+    color: black;
+    background-color: var(--subBlue);
+    box-shadow: 1vw 1vh var(--subYellow);
+    border: solid var(--primeRed);
+    border-width: 0.5vh;
+}
+#displaced:hover {
+    cursor: pointer;
+    filter: hue-rotate(180deg);
+}
+</style>
 <script>
 export default {
     methods: {
         colorShift() {
-            const border = document.querySelector(".bord0");
-            border.classList.toggle("bord1");
+            this.toggle = !this.toggle
+        }
+    },
+    watch: {
+        toggle: {
+            handler: function () {
+                this.$nextTick(() => {
+                    if (this.toggle) {
+                        this.root.style.setProperty("--bg", "--bgMain")
+                    } else {
+                        this.root.style.setProperty("--bg", "--bgAlt")
+                    }
+                }
+                )
+            }
+        }
+    },
+    data() {
+        return {
+            toggle: false,
         }
     },
 }
