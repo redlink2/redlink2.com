3c9c408b5e8608396323a0b409394911c95c8cb5
Ashton Redd <redlink2@pm.me>
2021-11-24 22:46:10 -0600

    Staging the creation of a whats new button.
    -plans are for it to store a cookie if youve seen it to prevent its displaying again, and to be itself a reminder that there are new additions to the website

---
+++ components/notif.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="notif">new shit</div>
+</template>
+<style>
+#notif {
+    display: flex;
+    justify-content: center;
+    z-index: 2;
+    position: fixed;
+    top: 88vh;
+    right: 88vw;
+    height: 7vh;
+    width: 8vw;
+    background-color: red;
+    border: solid;
+}
+</style>
