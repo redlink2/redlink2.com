20fd01744276f5c07a113bafef443bcddd50af84
Ashton Redd <redlink2@pm.me>
2021-12-07 03:23:09 -0600

    Repurposed notif.vue for email notifcation
    -now displays announcement of new email!

--- components/notif.vue
+++ components/notif.vue
@@ -1,17 +1,36 @@
 <template>
-    <div id="notif">new shit</div>
+    <div id="notif">
+        <p class="notif-text">
+            email me at
+            <a
+                class="notif-link"
+                href="mailto:redlink@redlink2.com"
+            >redlink@redlink2.com!</a>
+        </p>
+    </div>
 </template>
-<style>
+<style scoped>
 #notif {
-    display: flex;
-    justify-content: center;
-    z-index: 2;
+    height: 8vh;
+    top: 86.5vh;
+    left: 2vw;
+    right: 76vw;
+    margin-left: 2vw;
+    margin-right: 2vw;
+    margin-bottom: 5vh;
+    padding-bottom: 1vh;
+    line-break: strict;
     position: fixed;
-    top: 88vh;
-    right: 88vw;
-    height: 7vh;
-    width: 8vw;
-    background-color: red;
-    border: solid;
+    background-color: var(--subYellow);
+    border: solid 0.5vh;
+    border-color: var(--primeRed);
+}
+.notif-text {
+    font-size: 1.3em;
+    color: crimson;
+}
+.notif-link {
+    text-decoration: dotted;
+    color: navy;
 }
 </style>
