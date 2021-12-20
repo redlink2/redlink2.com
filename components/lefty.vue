ced2e3ee5bbe9bf6722bd45dee304ae88813ec47
Ashton Redd <redlink2@pm.me>
2021-12-20 04:30:14 -0600

    Added new-guest functions. -a cookie is made on first visit -special text will be visible in lefty while this cookie == false -on your third visit the cookie == true -once the cookie == true then normal anagram text will be seen in lefty

--- components/lefty.vue
+++ components/lefty.vue
@@ -4,20 +4,52 @@
 
 <script>
 export default {
-	// it takes approx 14.5-16 seconds for text to pass viewable area on std monitor
-	// need to add method to change to another phrase after passing
 	mounted() {
-		let roll = Math.floor(Math.random() * this.phrases.length)
-		let chars = this.phrases[roll]
-		const charsLen = chars.length;
-		// at 74 length, it seems to cut off characters.
-		for (var i = 0; i < 64; i++) {
-			this.randy += chars.charAt(Math.floor(Math.random() * charsLen));
-			document.querySelector('#lefty').innerHTML = `<h1>` + this.randy + `</h1>`;
+		this.fromHere()
+		if (this.pardner == true) {
+			this.anagrammer()
+		} else {
+			this.notFromHere()
+		}
+	},
+	methods: {
+		fromHere() {
+			if (!localStorage.getItem('reg')) {
+				localStorage.setItem('reg', false)
+			} else {
+				let cookie = localStorage.getItem('reg')
+				if (cookie == "true") {
+					this.pardner = true
+				} else {
+					localStorage.setItem('reg', true)
+				}
+			}
+		},
+		notFromHere() {
+			document.querySelector('#lefty').innerHTML = `<h1>` + this.stranger[Math.floor(Math.random() * this.stranger.length)] + `</h1>`
+
+		},
+		anagrammer() {
+			// it takes approx 14.5-16 seconds for text to pass viewable area on std monitor
+			// need to add method to change to another phrase after passing
+			let roll = Math.floor(Math.random() * this.phrases.length)
+			let chars = this.phrases[roll]
+			const charsLen = chars.length
+			// at 74 length, it seems to cut off characters.
+			for (var i = 0; i < 64; i++) {
+				this.randy += chars.charAt(Math.floor(Math.random() * charsLen))
+				document.querySelector('#lefty').innerHTML = `<h1>` + this.randy + `</h1>`
+			}
 		}
 	},
 	data() {
 		return {
+			pardner: false,
+			stranger: [
+				"You're not from 'round here are you pardner?",
+				"This site is mostly RNG so refresh for changes!",
+				"Email me your qualms at redlink@redlink2.com!"
+			],
 			phrases: [
 				"redlink2",
 				"permutations",
