08e75b192d04239c43fc5842534644096daa901e
Ashton Redd <redlink2@pm.me>
2021-12-20 04:27:58 -0600

    Changed search placeholder text to be generated. -selects placeholder text randomly from a array of song lyrics

--- components/search.vue
+++ components/search.vue
@@ -1,21 +1,30 @@
 <template>
 	<div id="search">
 		<form role="search" id="form">
-			<input
-				id="bar"
-				autocomplete="on"
-				type="text"
-				title="Search"
-				autofocus
-				placeholder="Explore That Sea"
-			/>
+			<input id="bar" autocomplete="on" type="text" title="Search" :placeholder="choice" />
 		</form>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			choice: "",
+			opts: [
+				"We're riding on the internet!",
+				"Cyberspace",
+				"Set free!",
+				"Hello Virtual Reality",
+				"Interactive appetite",
+				"Searching for a webite",
+				"Take the spin",
+				"Surfin' on the internet!"
+			],
+		}
+	},
 	mounted() {
+		this.choice = this.opts[Math.floor(Math.random() * this.opts.length)]
 		const bar = document.getElementById("bar");
 		const form = document.getElementById("form");
 		const link = "https://duckduckgo.com/?q=";
