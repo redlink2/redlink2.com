d953155930c3c57a73ab25cbbd535b18a3c20de9
Ashton Redd <redlink2@pm.me>
2021-12-19 22:07:24 -0600

    Staging ideas for API.vue. -plans to use database for color fetching

--- pages/api.vue
+++ pages/api.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
+        <h1>API Tests</h1>
+        <div id="tester">
+            <p v-if="$fetchState.pending">Fetching...</p>
+            <p v-else-if="$fetchState.error">An error occurred :(</p>
+            <div v-else>
+                <h2>Tester Get</h2>
+                <ul>
+                    <li v-for="user of users">{{ user.name }} - {{ user.phrase }} - {{ user.etc }}</li>
+                </ul>
+                <button @click="$fetch">Refresh</button>
+            </div>
+        </div>
         <div>
             <p v-if="$fetchState.pending">Fetching...</p>
             <p v-else-if="$fetchState.error">An error occurred :(</p>
             <div v-else>
-                <h1>API Test</h1>
+                <h2>API Test</h2>
                 <ul>
-                    <li v-for="user of users">{{ user.name }}</li>
+                    <!-- <li v-for="user of users">{{ user.name }} - {{ user.phrase }} - {{ user.etc }}</li> -->
+                    <div id="apiColor" :style="color">{{ color }}</div>
                 </ul>
-                <button @click="$fetch">Refresh</button>
             </div>
         </div>
     </div>
@@ -17,15 +29,24 @@
 export default {
     data() {
         return {
-            users: []
+            users: [],
+            colors: [],
+            color: String,
+            style: "background-color: "
         }
     },
     async fetch() {
         this.users = []
         this.users = await fetch(
-            'https://rl2-chaotic.com/users'
+            'https://rl2-chaotic.com/tester'
         ).then(res => res.json())
-    }
+
+        this.colors = []
+        this.colors = await fetch(
+            'https://rl2-chaotic.com/tester'
+        ).then(res => res.json())
+        this.color = this.style + this.colors[0].etc
+    },
 }
 </script>
 <style>
